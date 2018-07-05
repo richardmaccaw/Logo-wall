1aec99c97a9093b33e260d99d261a0174df21ea6
Richard MacCaw <32118670+richardmaccaw@users.noreply.github.com>
2018-07-05 12:14:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,64 @@
-<!DOCTYPE html>
+let domains = []
+let errors = []
+let greyscale = []
 
-<html>
-    <head>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-        <title>Logo Wall</title>
-        <link href="/styles.css" rel="stylesheet">
-        <link rel="icon" type="image/png" href="favicon.png">
-    </head>
-    <body>
-        <div class="bg-color">
-            <div class="flex-container">
-                <img src="/logowall.png" alt="Logowall">
-                <h4>Logo wall</h1>
-            </div>
-            <h2>Make a beautiful logo wall in seconds</h2>
-            <div id="powered-by">Logos by <a href='www.clearbit.com' target='blank'>Clearbit</a></div>
-                    
-            <form id="textareaForm">
-                <textarea name='textarea' rows='3' cols='80' required placeholder='Type / paste in some websites on separate lines to get started...'></textarea>
-                <br><input class="button" type="submit">
-                <button class ="button" id="clearButton">Clear</button>
-                <label class='checkbox'><input type="checkbox" id="greyscale" unchecked> Greyscale</label>
-            </form>
-        </div>
-        
-        <div id='logos' class='flex-container'></div>
-        
-        <script src="app.js"></script>       
-    </body>
-</html>
+// submit button function
+document.querySelector('#textareaForm').addEventListener('submit', function (e) {
+    e.preventDefault()
+    removeLogos()
+    domains = e.target.elements.textarea.value.split('\n')
+    getChecked()
+    getLogos(domains)
+    const scrollToLogos = document.querySelector("#logos")
+    scrollToLogos.scrollIntoView({behavior: 'smooth'})
+}) 
+
+// clear button function
+document.querySelector('#clearButton').addEventListener('click', function (e) {
+    e.preventDefault()
+    removeLogos()
+})
+
+// set greyscale button
+document.querySelector('#greyscale').addEventListener('change', function (e) {
+    e.preventDefault()
+    removeLogos()
+    const checkCheckbox = document.getElementById("greyscale").checked
+    if (checkCheckbox) {
+        greyscale = '&greyscale=true'
+        getLogos(domains)
+    } else {
+        greyscale = []
+        getLogos(domains)
+    }  
+})
+
+
+// Get logos
+const getLogos = function (domains) {
+    domains.forEach((domain) => {
+        const logo = document.createElement('img')
+        logo.id = 'logo'
+        logo.src = 'https://logo.clearbit.com/' + domain + '?size=200' + greyscale       
+        logo.onerror = function () {
+            logo.remove()
+            errors.push(domain)
+        }
+        document.querySelector('#logos').appendChild(logo)
+    })
+}
+
+// Remove logos
+const removeLogos = () => {
+    document.querySelectorAll('#logo').forEach(e => e.parentNode.removeChild(e))
+}
+
+// Check greyscale checkbox
+getChecked = () => {
+    const checkCheckbox = document.getElementById("greyscale").checked
+    if (checkCheckbox) {
+        greyscale = '&greyscale=true'
+    } else {
+        greyscale = []
+    }  
+}
